cf68f24511c28d8425779a3149efcaebe02fa8b4
Urvi <urviporwal@gmail>
2026-07-15 21:50:02 +0530

    Added FastAPI task API

---
+++ main.py
@@ -0,0 +1,79 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+tasks = [
+    {"id": 1, "title": "Learn FastAPI", "done": False},
+    {"id": 2, "title": "Build CRUD API", "done": False},
+    {"id": 3, "title": "Submit Assignment", "done": False},
+]
+
+@app.get("/")
+def home():
+    return {"name": "Task API",
+    "version": "1.0",
+    "endpoints": ["/tasks"]}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    raise HTTPException(
+    status_code=404,
+    detail=f"Task {task_id} not found"
+)
+
+class Task(BaseModel):
+    title: str
+    done: bool
+
+@app.post("/tasks", status_code=201)
+def create_task(task: Task):
+    if task.title.strip() == "":
+        raise HTTPException(
+        status_code=400,
+        detail="Title cannot be empty"
+        )
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": task.title,
+        "done": False
+    }
+
+    tasks.append(new_task)
+    return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: Task):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
